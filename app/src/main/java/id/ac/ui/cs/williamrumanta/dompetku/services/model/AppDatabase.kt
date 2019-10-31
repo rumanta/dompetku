e8a6842fd0d085f6e7b7fa4ccbb79eff0aa37580
William Rumanta <rumantawilliam@yahoo.co.id>
2019-10-31 16:36:56 +0700

    Add roomCallback for asynctask populatedb

--- app/src/main/java/id/ac/ui/cs/williamrumanta/dompetku/services/model/AppDatabase.kt
+++ app/src/main/java/id/ac/ui/cs/williamrumanta/dompetku/services/model/AppDatabase.kt
@@ -1,28 +1,56 @@
 package id.ac.ui.cs.williamrumanta.dompetku.services.model
 
 import android.content.Context
+import android.os.AsyncTask
+import androidx.annotation.NonNull
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import java.util.*
 
-@Database(entities = arrayOf(Transaction::class), version = 1)
+@Database(entities = [Transaction::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun transactionDao(): TransactionDao
-    private var instance: AppDatabase? = null
 
+    companion object {
+        private lateinit var instance: AppDatabase
 
-    @Synchronized
-    fun getInstance(context: Context): AppDatabase {
-        if (instance == null) {
-            instance = Room.databaseBuilder(
-                context.getApplicationContext(),
-                AppDatabase::class.java, "transaction_database"
-            )
-                .fallbackToDestructiveMigration()
-                .build()
+        @Synchronized
+        fun getInstance(context: Context): AppDatabase {
+            if (instance == null) {
+                instance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    AppDatabase::class.java, "app_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
+                    .build()
+            }
+
+            return instance
         }
 
-        return instance as AppDatabase
-    }
+        private val roomCallback = object : RoomDatabase.Callback() {
+            override fun onCreate(@NonNull db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+                PopulateDBAsyncTask(instance).execute()
+            }
+        }
 
+        private class PopulateDBAsyncTask constructor(db: AppDatabase) :
+            AsyncTask<Void, Void, Void>() {
+            private val transactionDao: TransactionDao
+
+            init {
+                transactionDao = db.transactionDao()
+            }
+
+
+            override fun doInBackground(vararg voids: Void): Void? {
+                transactionDao.insert(Transaction(1, "asd", 50000.0, 2, Date()))
+                return null
+            }
+        }
+    }
 }
